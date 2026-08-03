ab6a838c2d4cd58c7252203faf0c3b7801b5544c
uniconsulta <marcosdalleprane2@gmail.com>
2026-08-03 14:18:02 -0300

    att

--- app.js
+++ app.js
@@ -309,7 +309,7 @@ function criarCard(item) {
       ${item.status || "Verificar"}
     </span>
 
-    <h3>${item.Atividade || "Atividade"}</h3>
+    <h3>${(item.aula || "Aula").replace(/^Aula/i, "Atividade")}</h3>
 
     <div class="meta">
       <strong>${item.data || "Data não informada"}</strong><br>
